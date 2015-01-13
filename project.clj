9b22955f2a4c4b3e1218df405e4c008432801fa4
Mike Blume <michael.blume@climate.com>
2015-01-12 17:32:00 -0800

    for convenience, include a project.clj
    mostly for nREPL

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject stuff "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
+  :test-paths ["test"]
+  :dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]
+                 [org.clojure/test.generative "0.5.0"]
+                 [org.clojure/test.check "0.6.1"]])
